c304acbbf1e5f142c83f3d36e8a0ce597c7e0da1
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2023-11-08 18:19:12 -0500

    refactor: Encapsulate tokenization functions into Tokenizer class
    - Introduced a new `Tokenizer` class to encapsulate tokenization methods
    - Stored the tokenization regular expression as a class variable `TOKEN_REGEX`
    - Converted `subword_tokenize`, `word_tokenize`, `tokenize`, `encode_token_to_bytes`, and `decode_bytes_to_token` into static methods within the class
    - Implemented `zfill(2)` for padding hex strings to an even length in `decode_from_bytes`
    - Updated the main test case to use a Japanese string, testing Unicode handling
    - Adjusted the main block to demonstrate the new class method invocations
    
    This commit organizes tokenization functionalities within a class structure, enhancing code readability and maintainability.

--- vektor/token.py
+++ vektor/token.py
@@ -5,53 +5,44 @@ import binascii
 import re
 
 
-# TODO: Refine the subword tokenization logic
-def subword_tokenize(text, split_length=5):
-    # Split the text into subwords of the specified length
-    return [text[i : i + split_length] for i in range(0, len(text), split_length)]
+class Tokenizer:
+    # Compile the regular expression for tokenization
+    TOKEN_REGEX = re.compile(r"\w+|[^\w\s]|'\w+", re.UNICODE)
 
+    @staticmethod
+    def tokenize(text):
+        """Tokenize the input text into a list of tokens."""
+        return Tokenizer.TOKEN_REGEX.findall(text)
 
-# TODO: Refine the word tokenization logic
-def word_tokenize(text):
-    # Improved regex to handle contractions, URLs, etc.
-    tokens = re.findall(r"\w+|[^\w\s]|'\w+", text, re.UNICODE)
-    return tokens
+    @staticmethod
+    def encode_to_bytes(token):
+        """Encode the token to its byte representation."""
+        try:
+            return int(binascii.hexlify(token.encode()), 16)
+        except UnicodeEncodeError as e:
+            error_msg = f"Encoding failed for token: {token}"
+            # Consider using logging.error(error_msg)
+            raise UnicodeEncodeError(error_msg) from e
 
-
-# Define a function to tokenize a string into words, subwords, and punctuation as separate tokens
-def tokenize(text):
-    # First, tokenize by words using the refined word_tokenize function
-    word_tokens = word_tokenize(text)
-    # Then, for each word token, perform subword tokenization
-    tokens = [subword for token in word_tokens for subword in subword_tokenize(token)]
-    return tokens
-
-
-# Add error handling to encoding and decoding functions
-def encode_token_to_bytes(token):
-    try:
-        return int(binascii.hexlify(token.encode()), 16)
-    except UnicodeEncodeError as e:
-        # Handle the specific error, perhaps log it or re-raise with additional information
-        raise UnicodeEncodeError(f"Encoding failed for token: {token}") from e
-
-
-def decode_bytes_to_token(encoded_token):
-    try:
-        hex_str = format(encoded_token, "x")  # Convert to hex string
-        hex_str = "0" * (len(hex_str) % 2) + hex_str
-        return binascii.unhexlify(hex_str).decode()
-    except (binascii.Error, ValueError) as e:
-        # Handle decoding errors
-        raise ValueError(f"Decoding failed for encoded token: {encoded_token}") from e
+    @staticmethod
+    def decode_from_bytes(encoded_token):
+        """Decode the encoded token back to its string representation."""
+        try:
+            # Ensures that hex_str is padded to even length
+            hex_str = format(encoded_token, "x").zfill(2)
+            return binascii.unhexlify(hex_str).decode()
+        except (binascii.Error, ValueError) as e:
+            error_msg = f"Decoding failed for encoded token: {encoded_token}"
+            # Consider using logging.error(error_msg)
+            raise ValueError(error_msg) from e
 
 
 if __name__ == "__main__":
     # Testing the encoding and decoding
-    text = "Ye are a baggage: the Slys are no rogues; look in the chronicles; we came in with Richard Conqueror. Therefore paucas pallabris; let the world slide: sessa!"
-    tokens = tokenize(text)
-    encoded_tokens = [encode_token_to_bytes(token) for token in tokens]
-    decoded_tokens = [decode_bytes_to_token(token) for token in encoded_tokens]
+    text = "こんにちは！ これは単なるテストです。"
+    tokens = Tokenizer.tokenize(text)
+    encoded_tokens = [Tokenizer.encode_to_bytes(token) for token in tokens]
+    decoded_tokens = [Tokenizer.decode_from_bytes(token) for token in encoded_tokens]
 
     print(f"Original text: {text}")
     print(f"Original tokens: {tokens}")
