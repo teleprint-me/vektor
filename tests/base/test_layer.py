192e6a0e7bbd74cb26ab8343393ebcb9b8972ec8
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2023-11-09 22:45:08 -0500

    test: Update Layer Tests for Sparse Array Compatibility
    - Modified StubLayer to utilize the generic TArray type for input and output data
    - Adjusted layer_input and layer_gradient fixtures to test with both ndarray and csr_array types
    - Updated test_layer_forward and test_layer_backward to handle assertions for both dense and sparse arrays
    
    These updates ensure that our layer tests cover the new functionality introduced for handling sparse arrays in neural network layers.

--- tests/base/test_layer.py
+++ tests/base/test_layer.py
@@ -3,37 +3,46 @@ tests/base/test_layer.py
 """
 import numpy as np
 import pytest
+from scipy.sparse import csr_array
 
-from vektor.base.layer import Layer
+from vektor.base.layer import Layer, TArray
 
 
-class StubLayer(Layer):
-    def forward(self, input_data: np.ndarray) -> np.ndarray:
+class StubLayer(Layer[TArray]):
+    def forward(self, input_data: TArray) -> TArray:
         # A simple pass-through for testing purposes
         return input_data
 
-    def backward(self, output_gradient: np.ndarray, **kwargs) -> np.ndarray:
+    def backward(self, output_gradient: TArray, **kwargs) -> TArray:
         # For testing, we'll simply return the output_gradient
         return output_gradient
 
 
-@pytest.fixture
-def layer_input():
-    return np.array([[1, 2, 3], [4, 5, 6]], dtype=float)
+@pytest.fixture(params=[np.array, csr_array])
+def layer_input(request):
+    data = np.array([[1, 2, 3], [4, 5, 6]], dtype=float)
+    return request.param(data)
 
 
-@pytest.fixture
-def layer_gradient():
-    return np.array([[0.1, 0.2, 0.3], [0.4, 0.5, 0.6]], dtype=float)
+@pytest.fixture(params=[np.array, csr_array])
+def layer_gradient(request):
+    data = np.array([[0.1, 0.2, 0.3], [0.4, 0.5, 0.6]], dtype=float)
+    return request.param(data)
 
 
 def test_layer_forward(layer_input):
     layer = StubLayer()
     output = layer.forward(layer_input)
-    np.testing.assert_array_equal(output, layer_input)
+    if isinstance(output, np.ndarray):
+        np.testing.assert_array_equal(output, layer_input)
+    else:  # it's a sparse matrix
+        np.testing.assert_array_equal(output.toarray(), layer_input.toarray())
 
 
 def test_layer_backward(layer_input, layer_gradient):
     layer = StubLayer()
     output = layer.backward(layer_gradient)
-    np.testing.assert_array_equal(output, layer_gradient)
+    if isinstance(output, np.ndarray):
+        np.testing.assert_array_equal(output, layer_gradient)
+    else:  # it's a sparse matrix
+        np.testing.assert_array_equal(output.toarray(), layer_gradient.toarray())
