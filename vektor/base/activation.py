0faf0cab6d1df1e5759244bdc6397870377690fc
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2023-11-10 00:41:27 -0500

    enhance: Support Sparse Arrays in Activation Layer
    - Upgraded the Activation class to be generic, allowing it to handle both ndarray and sparray
    - Modified the activation and activation_prime functions to operate on the generic TArray type
    - Added handling in backward method for both dense and sparse array multiplication
    - Stored the input and output of the activation layer as optional generic TArray for later use
    
    The update accommodates the new array interface from SciPy, enabling our activation layers to work with both dense and sparse arrays efficiently.

--- vektor/base/activation.py
+++ vektor/base/activation.py
@@ -6,61 +6,70 @@ This module defines the base class for all activation layers used in neural netw
 from typing import Any, Callable, Mapping, Optional
 
 from numpy import multiply, ndarray
+from scipy.sparse import sparray
 
-from vektor.base.layer import Layer
+from vektor.base.layer import Layer, TArray
 
 
-class Activation(Layer):
+class Activation(Layer[TArray]):
     """
     Base class for activation layers in neural networks.
     """
 
     def __init__(
         self,
-        activation: Callable[[ndarray], ndarray],
-        activation_prime: Callable[[ndarray], ndarray],
+        activation: Callable[[TArray], TArray],
+        activation_prime: Callable[[TArray], TArray],
     ):
         """
         Initializes the Activation layer.
 
         Args:
-            activation (Callable[[ndarray], ndarray]): The activation function.
-            activation_prime (Callable[[ndarray], ndarray]): The derivative of the activation function.
+            activation (Callable[[TArray], TArray]): The activation function.
+            activation_prime (Callable[[TArray], TArray]): The derivative of the activation function.
         """
         self.activation = activation
         self.activation_prime = activation_prime
+        self.input: Optional[TArray] = None
+        self.output: Optional[TArray] = None
 
-        self.input: Optional[ndarray] = None
-        self.output: Optional[ndarray] = None
-
-    def forward(self, input_data: ndarray) -> ndarray:
+    def forward(self, input_data: TArray) -> TArray:
         """
         Perform a forward pass through the activation layer.
 
         Args:
-            input_data (numpy.ndarray): The input data to the layer.
+            input_data (TArray): The input data to the layer.
 
         Returns:
-            numpy.ndarray: The output data from the layer, after applying the activation function.
+            TArray: The output data from the layer, after applying the activation function.
         """
         self.input = input_data
         self.output = self.activation(self.input)
         return self.output
 
-    def backward(
-        self, output_gradient: ndarray, **kwargs: Mapping[str, Any]
-    ) -> ndarray:
+    def backward(self, output_gradient: TArray, **kwargs: Mapping[str, Any]) -> TArray:
         """
         Perform a backward pass to compute the gradient through the activation layer.
 
         Args:
-            output_gradient (numpy.ndarray): The gradient of the loss with respect to the output.
+            output_gradient (TArray): The gradient of the loss with respect to the output.
             kwargs (Mapping[str, Any]): Additional keyword arguments for more specialized layers.
 
         Returns:
-            numpy.ndarray: The gradient of the loss with respect to the input, after applying the derivative of the activation function.
+            TArray: The gradient of the loss with respect to the input, after applying the derivative of the activation function.
         """
         if self.input is None:
             raise ValueError("forward must be called before backward.")
 
-        return multiply(output_gradient, self.activation_prime(self.input))
+        # Ensure the multiplication is compatible with both dense and sparse arrays
+        if isinstance(output_gradient, sparray):
+            # If output_gradient is sparse, use the multiplication that's appropriate for sparse matrices
+            return output_gradient.multiply(self.activation_prime(self.input))
+        elif isinstance(output_gradient, ndarray):
+            # Use numpy's multiply for dense arrays
+            print(output_gradient, self.input)
+            return multiply(output_gradient, self.activation_prime(self.input))
+        else:
+            raise ValueError(
+                f"Unknown array type. Expected ndarray or sparray. Got {type(output_gradient)} instead."
+            )
