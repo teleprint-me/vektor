56bfb3d9b8a6a33784f27e6ea77724999505f3d0
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2023-11-09 22:43:57 -0500

    refactor: Extend Layer Protocol to Support Sparse Arrays
    - Introduced a type variable, TArray, to accept both ndarray and sparray types
    - Updated the Layer Protocol to be generic, supporting the new TArray type
    - Added forward and backward method type hints to use TArray
    - Implemented new get_params, set_params, and update_params methods for layer parameters handling
    
    The changes follow SciPy's recommendation to transition to array interfaces from the older matrix interface, ensuring compatibility with new sparse array types.

--- vektor/base/layer.py
+++ vektor/base/layer.py
@@ -2,40 +2,62 @@
 vektor/base/layer.py
 
 This module defines the base class for all layers used in neural networks.
+
+NOTE:
+
+SciPy is switching to an array interface, compatible with NumPy arrays, from the older matrix interface. SciPy recommends using array objects (csr_array, bsr_array, coo_array, etc.) for all new work.
+
+sparray is the new base class for SciPy array-like objects which are compatible with ndarray's.
+
+Source: https://docs.scipy.org/doc/scipy-1.11.3/reference/sparse.html
 """
-from typing import Any, Mapping, Protocol
+from typing import Any, Mapping, Protocol, TypeVar
 
 from numpy import ndarray
+from scipy.sparse import sparray
+
+# Define a type variable that can be ndarray or any subtype of sparray (like csr_array, coo_array, etc.)
+TArray = TypeVar("TArray", ndarray, sparray)
 
 
-class Layer(Protocol):
+class Layer(Protocol[TArray]):
     """
     Protocol defining the mandatory methods that any neural network layer must implement.
     """
 
-    def forward(self, input_data: ndarray) -> ndarray:
+    def forward(self, input_data: TArray) -> TArray:
         """
         Perform a forward pass through the layer.
 
         Args:
-            input_data (numpy.ndarray): The input data to the layer.
+            input_data (TArray): The input data to the layer.
 
         Returns:
-            numpy.ndarray: The output data from the layer.
+            TArray: The output data from the layer.
         """
         ...
 
-    def backward(
-        self, output_gradient: ndarray, **kwargs: Mapping[str, Any]
-    ) -> ndarray:
+    def backward(self, output_gradient: TArray, **kwargs: Mapping[str, Any]) -> TArray:
         """
         Perform a backward pass to compute the gradient.
 
         Args:
-            output_gradient (numpy.ndarray): The gradient of the loss with respect to the output.
+            output_gradient (TArray): The gradient of the loss with respect to the output.
             kwargs (Mapping[str, Any]): Additional keyword arguments for more specialized layers.
 
         Returns:
-            numpy.ndarray: The gradient of the loss with respect to the input.
+            TArray: The gradient of the loss with respect to the input.
         """
+        ...
+
+    def get_params(self) -> Mapping[str, TArray]:
+        """Return the parameters of the layer."""
+        ...
+
+    def set_params(self, **params: TArray) -> None:
+        """Set the parameters of the layer."""
+        ...
+
+    def update_params(self, **grads: TArray) -> None:
+        """Update the parameters of the layer using the provided gradients."""
         ...
