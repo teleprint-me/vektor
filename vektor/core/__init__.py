ce3b4b4e4077886c600fe47dc1b60dc1ce134587
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2023-11-16 10:54:49 -0500

    add: New __init__.py to vektor/core package
    - Added a new '__init__.py' file to the 'vektor/core' package.
    
    This addition is for organizing the code within the 'vektor/core' package.

---
+++ vektor/core/__init__.py
@@ -0,0 +1,8 @@
+"""
+vektor/core/__init__.py
+
+There's nothing more dystopian than cherry picking ideas and having individuals or groups unilaterally deciding who gets to think, know, and say what. We might as well destroy all knowledge, end any pursuit towards it, and settle for absolute tyranny at that point. That, or we can choose otherwise. If only it were this black and white. It isn't as simple as a stark contrast. There's depth and complexity... hidden layers... It's business as usual.
+"""
+from vektor.core.vektor import Vektor
+
+__all__ = [Vektor]
