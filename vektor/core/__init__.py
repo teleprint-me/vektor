d329a45840cc8d35291d5c52b29df1af7ac77afc
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2023-12-06 20:08:02 -0500

    add: Added type definitions import from vektor.core.dtype
    Signed-off-by: teleprint-me <77757836+teleprint-me@users.noreply.github.com>

--- vektor/core/__init__.py
+++ vektor/core/__init__.py
@@ -1,8 +1,20 @@
 """
 vektor/core/__init__.py
 
-There's nothing more dystopian than cherry picking ideas and having individuals or groups unilaterally deciding who gets to think, know, and say what. We might as well destroy all knowledge, end any pursuit towards it, and settle for absolute tyranny at that point. That, or we can choose otherwise. If only it were this black and white. It isn't as simple as a stark contrast. There's depth and complexity... hidden layers... It's business as usual.
+"It is possible that I am dreaming right now and that all of my perceptions are false."
+    - Rene Descartes
 """
+from vektor.core.dtype import (
+    TBool,
+    TComplex,
+    TFloat,
+    TInt,
+    TMatrix,
+    TNoise,
+    TScalar,
+    TShape,
+    TTensor2D,
+    TTensor3D,
+    TVector,
+)
 from vektor.core.vektor import Vektor
-
-__all__ = [Vektor]
