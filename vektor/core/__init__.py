5570350e207bc63f30c246e26b9860da48741187
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2023-12-09 22:11:34 -0500

    refactor: Update imports in vektor.core
    - Updated import statements for consistency and to fix broken imports.
    - Reorganized import statements for improved readability.
    
    These changes ensure that the module's imports are consistent and correctly reference the required classes and types.

--- vektor/core/__init__.py
+++ vektor/core/__init__.py
@@ -1,20 +1,9 @@
 """
-vektor/core/__init__.py
+Module: vektor.core
 
 "It is possible that I am dreaming right now and that all of my perceptions are false."
     - Rene Descartes
 """
-from vektor.core.dtype import (
-    TBool,
-    TComplex,
-    TFloat,
-    TInt,
-    TMatrix,
-    TNoise,
-    TScalar,
-    TShape,
-    TTensor2D,
-    TTensor3D,
-    TVector,
-)
-from vektor.core.vektor import Vektor
+from vektor.core.dtype import TMatrix, TScalar, TShape, TTensor, TTensorND, TVector
+from vektor.core.matrix import Matrix
+from vektor.core.vector import Vector
