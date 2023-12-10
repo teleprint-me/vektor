bcc7cd8d1532c0d6043e59c6cda96aab3585d3ef
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2023-12-09 20:28:55 -0500

    feat: Add Initial Placeholder for vektor.core.vector Module
    Added the `vektor.core.vector` module, which serves as an initial placeholder for handling vectors. This module is currently empty but will be developed further in future updates.

---
+++ vektor/core/vector.py
@@ -0,0 +1,15 @@
+"""
+Module: vektor.core.vector.py
+"""
+from typing import List, Optional, Tuple, Type
+
+
+class Vector:
+    def __init__(
+        self,
+        vector: Optional[List[float]] = None,
+        shape: Optional[Tuple[int, int]] = None,
+        dtype: Type = float,
+    ) -> None:
+        """ """
+        ...
