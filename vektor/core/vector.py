43441b7502816f34cea9a1612829ff83a0ec562c
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2023-12-09 22:25:59 -0500

    feat: Redefine Vector class for enhanced flexibility and functionality
    - Updated the Vector class to accept a variable number of coordinates, allowing for vectors of different dimensions.
    - Added dunder methods like __add__, __sub__, and others for vector operations.
    - Introduced a length property to calculate the magnitude of the vector.
    
    These changes provide a more versatile and powerful foundation for vector operations within the Vektor package.

--- vektor/core/vector.py
+++ vektor/core/vector.py
@@ -1,15 +1,30 @@
 """
 Module: vektor.core.vector.py
 """
-from typing import List, Optional, Tuple, Type
+from vektor.core.dtype import TScalar, TVector
 
 
 class Vector:
-    def __init__(
-        self,
-        vector: Optional[List[float]] = None,
-        shape: Optional[Tuple[int, int]] = None,
-        dtype: Type = float,
-    ) -> None:
-        """ """
-        ...
+    def __init__(self, *coordinates: TScalar) -> None:
+        self.coordinates = coordinates
+
+    def __repr__(self) -> str:
+        return f"Vector{self.coordinates}"
+
+    def __add__(self, other: "Vector") -> "Vector":
+        if len(self.coordinates) != len(other.coordinates):
+            raise ValueError("Vectors must have the same dimension for addition")
+        return Vector(*(a + b for a, b in zip(self.coordinates, other.coordinates)))
+
+    def __sub__(self, other: "Vector") -> "Vector":
+        if len(self.coordinates) != len(other.coordinates):
+            raise ValueError("Vectors must have the same dimension for subtraction")
+        return Vector(*(a - b for a, b in zip(self.coordinates, other.coordinates)))
+
+    # Additional dunder methods like __mul__ for dot product, scalar multiplication, etc.
+
+    @property
+    def length(self) -> float:
+        return sum(x**2 for x in self.coordinates) ** 0.5
+
+    # Other vector-specific methods...
