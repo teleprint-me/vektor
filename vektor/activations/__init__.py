d98270f1519e838267a009fbf02222df2640dfa9
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-01-24 20:01:32 -0500

    feat: Add vektor/activations/__init__.py
    - Added the vektor/activations/__init__.py file.
    - This file initializes and exposes specific activation functions (ReLU, Sigmoid, Tanh) from submodules within the activations directory.
    - Organized activation functions for easy access and import.

---
+++ vektor/activations/__init__.py
@@ -0,0 +1,8 @@
+"""
+tmnn/activations/__init__.py
+"""
+from tmnn.activations.relu import ReLU
+from tmnn.activations.sigmoid import Sigmoid
+from tmnn.activations.tanh import Tanh
+
+__all__ = [ReLU, Sigmoid, Tanh]
